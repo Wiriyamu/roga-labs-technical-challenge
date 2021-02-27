32aaf1579144f22c1f35f2b973bd2cdfe31439a9
Willian Silva <will.devfrontend@gmail.com>
2021-02-27 13:40:12 -0300

    feat: add reset css

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
-  <div></div>
+  <the-footer />
 </template>
 
 <script>
+import TheFooter from '@/components/TheFooter.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  components: { TheFooter }
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  outline: 0;
+  box-sizing: border-box;
+}
+
+html,
+body {
+  height: 100vh;
+}
+</style>
