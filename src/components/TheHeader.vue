6e81890409e5eb50a7590f47a3c4d70256229cc8
Willian Silva <will.devfrontend@gmail.com>
2021-02-27 15:12:48 -0300

    feat(the-header): add title, user info and dropdown

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,31 @@
 <template>
-  <nav class="navbar navbar-light bg-light">
-    <span class="navbar-brand mb-0 h1">Navbar</span>
+  <nav class="navbar navbar-dark bg-danger d-flex justify-content-between">
+    <span class="navbar-brand mb-0 ml-2 h1">RogaLags</span>
+
+    <div class="d-flex justify-content-between align-itens-center mr-5">
+      <div class="mr-2">
+        <h6 class="text-white">Jonas Joe</h6>
+        <p class="text-white">Último Login: 10/02/2021</p>
+      </div>
+
+      <div class="btn-group">
+        <button
+          type="button"
+          class="btn btn-outline-light dropdown-toggle"
+          data-toggle="dropdown"
+          data-display="static"
+          aria-haspopup="true"
+          aria-expanded="false"
+        ></button>
+        <div class="dropdown-menu dropdown-menu-lg-right">
+          <button class="dropdown-item" type="button">Action</button>
+          <button class="dropdown-item" type="button">Another action</button>
+          <button class="dropdown-item" type="button">
+            Something else here
+          </button>
+        </div>
+      </div>
+    </div>
   </nav>
 </template>
 
@@ -10,4 +35,8 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+img {
+  width: 80px;
+}
+</style>
